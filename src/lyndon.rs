a53edaa2c48d130a6a4ff3fc7e91983829fa344b
Léo Ackermann <leo.ackermann@inria.fr>
2025-05-21 20:01:44 +0200

    :sparkles: Support Lyndon enumeration of bounded size

--- src/lyndon.rs
+++ src/lyndon.rs
@@ -1,6 +1,7 @@
 
 type Letter = u8;
 
+#[derive(Debug)]
 struct LyndonWord {
     vec: Vec<Letter>,
     len: usize,
@@ -51,9 +52,9 @@ impl<'a> Iterator for LyndonWordIter<'a> {
                 }
             },
             false => {
-                // if let Err(_) = next_bllw(self.lw) {
-                //     self.stop_on_next_call = true;
-                // }
+                if let Err(_) = next_bllw(self.lw) {
+                    self.stop_on_next_call = true;
+                }
             },
         }
 
@@ -63,25 +64,50 @@ impl<'a> Iterator for LyndonWordIter<'a> {
 
 
 
+fn next_bllw(lw: &mut LyndonWord) -> Result<(), String> {
+    let n = lw.vec.len();
+    let i = lw.len;
 
-fn next_fllw(fllw: &mut LyndonWord) -> Result<(), String> {
+    if i == 1 && lw.vec[0] == lw.max_letter {
+        return Err("This was the greatest Lyndon word of this fixed length".to_string());
+    }
+
+    for k in i..n {
+        lw.vec[k] = lw.vec[k % i];
+    }
+
+    // Remove right trailing max letters, and increase the first non-max letter
+    let mut i = n;
+    while lw.vec[i - 1] == lw.max_letter {
+        i -= 1
+    }
+    lw.vec[i - 1] += 1;
+
+    lw.len = i;
+
+    Ok(())
+}
+
+
+
+fn next_fllw(lw: &mut LyndonWord) -> Result<(), String> {
         // Source. "Generation d'une section des classes de conjugaison et arbre
         // des mots de Lyndon de longueur bornee", Jean-Pierre DUVAL (1988)
         //
         // NOTE. Just shifted the indices by one when looking to a vector cell.
-        let n = fllw.len;
+        let n = lw.len;
 
         // Remove right trailing max letters, and increase the first non-max letter
         let mut i = n;
-        while fllw.vec[i - 1] == fllw.max_letter {
+        while lw.vec[i - 1] == lw.max_letter {
             i -= 1
         }
-        fllw.vec[i - 1] += 1;
+        lw.vec[i - 1] += 1;
 
         // If the first letter is maximal, then the current Lyndon word is
         // already the greatest of its size
-        if fllw.vec[1 - 1] == fllw.max_letter {
-            return Err("This was the greatest Lyndon word of this size".to_string());
+        if lw.vec[1 - 1] == lw.max_letter {
+            return Err("This was the greatest Lyndon word of this fixed length".to_string());
         }
 
         // Repeat the [0..k-1] pattern integrally, as many times as possible
@@ -89,7 +115,7 @@ fn next_fllw(fllw: &mut LyndonWord) -> Result<(), String> {
         let mut k = i;
         while i < n - k {
             for j in 1..=i {
-                fllw.vec[k + j - 1] = fllw.vec[j - 1]
+                lw.vec[k + j - 1] = lw.vec[j - 1]
             }
             k += i; // NOTE. There was a typo here in the source
         }
@@ -101,19 +127,19 @@ fn next_fllw(fllw: &mut LyndonWord) -> Result<(), String> {
         // NOTE. "Boucle 2" in the source
         while i != n {
             for j in 1..=(n - k) {
-                fllw.vec[k + j - 1] = fllw.vec[j - 1];
+                lw.vec[k + j - 1] = lw.vec[j - 1];
             }
             i = n;
-            while fllw.vec[i - 1] == fllw.max_letter {
+            while lw.vec[i - 1] == lw.max_letter {
                 i -= 1;
             }
-            fllw.vec[i - 1] += 1;
+            lw.vec[i - 1] += 1;
             let d = i - k;
 
             // NOTE. "Boucle 2.1" in the source
             while d <= n - i {
                 for jj in 1..=d {
-                    fllw.vec[i + jj - 1] = fllw.vec[k + jj - 1];
+                    lw.vec[i + jj - 1] = lw.vec[k + jj - 1];
                 }
                 i += d;
             }
@@ -123,18 +149,6 @@ fn next_fllw(fllw: &mut LyndonWord) -> Result<(), String> {
     }
 
 
-
-
-
-
-
-
-// struct fixedLenghtLW {
-//     vec: Vec<letter>,
-//     len: usize,
-// }
-
-
 #[test]
 fn test_fllw_smallest() {
     let fllw = LyndonWord::new_smallest(6, 1);
@@ -142,8 +156,8 @@ fn test_fllw_smallest() {
 }
 
 #[test]
-fn test_fllw_iterator() {
-    let lw42 = [
+fn test_fixed_length_lw_iterator() {
+    let fllw42 = [
         [0, 0, 0, 1], [0, 0, 0, 2], [0, 0, 1, 1], [0, 0, 1, 2],
         [0, 0, 2, 1], [0, 0, 2, 2], [0, 1, 0, 2], [0, 1, 1, 1],
         [0, 1, 1, 2], [0, 1, 2, 1], [0, 1, 2, 2], [0, 2, 1, 1],
@@ -151,8 +165,23 @@ fn test_fllw_iterator() {
         [1, 1, 2, 2], [1, 2, 2, 2]
     ];
 
-    let mut fllw = LyndonWord::new_smallest(4, 2);
-    assert_eq!(fllw.iter(true).collect::<Vec<_>>(), lw42);
+    let mut lw = LyndonWord::new_smallest(4, 2);
+    assert_eq!(lw.iter(true).collect::<Vec<_>>(), fllw42);
 }
 
+#[test]
+fn test_bounded_length_lw_iterator() {
+    let bllw42: Vec<Vec<Letter>> = vec![
+        vec![0], vec![0, 0, 0, 1], vec![0, 0, 0, 2], vec![0, 0, 1],
+        vec![0, 0, 1, 1], vec![0, 0, 1, 2], vec![0, 0, 2], vec![0, 0, 2, 1],
+        vec![0, 0, 2, 2], vec![0, 1], vec![0, 1, 0, 2], vec![0, 1, 1],
+        vec![0, 1, 1, 1], vec![0, 1, 1, 2], vec![0, 1, 2], vec![0, 1, 2, 1],
+        vec![0, 1, 2, 2], vec![0, 2], vec![0, 2, 1], vec![0, 2, 1, 1],
+        vec![0, 2, 1, 2], vec![0, 2, 2], vec![0, 2, 2, 1], vec![0, 2, 2, 2],
+        vec![1], vec![1, 1, 1, 2], vec![1, 1, 2], vec![1, 1, 2, 2],
+        vec![1, 2], vec![1, 2, 2], vec![1, 2, 2, 2], vec![2],
+    ];
 
+    let mut lw = LyndonWord::new_smallest(4, 2);
+    assert_eq!(lw.iter(false).collect::<Vec<_>>(), bllw42);
+}
